cf95380d5f27ba79dce7634ec82b7f1e17e352e5
Arley Prates <arleyprates@gmail.com>
2014-07-09 20:27:35 -0300

    Insere um elemento

--- untiledd.c
+++ untiledd.c
@@ -16,35 +16,47 @@ Deque* createItem () {
 	return novo;
 }
 
-int insereDireita (Deque **D1, int item) {
-	Deque *aux, *novo;
-	novo = createItem ();
-	
+int insereDireita (Deque *D1, int item) {
+	Deque *novo;
+	if (D1->item == 0) {
+		novo = createItem ();
+		novo->item = item;
+		D1 = novo;
+		return 1;	
+	} else {
+		while (D1->direita != NULL) {
+			D1->direita = D1->direita->direita;
+		}
+		novo = createItem ();
+		novo->item = item;
+		D1 = novo;
+		return 1;
+	}
 	/*if (D1 != NULL) {
 		novo->item = item;
 		D1 = novo;
 		printf ("%d\n", novo->item);
 		return 1;
 	}*/
-
+	novo = createItem ();
+	return 0;
 }
-/*
+
 void imprime (Deque D1) {
 	while (D1.direita != NULL) {
 		D1.direita = D1.direita->direita;
 		printf ("%d ", D1.item);
 	}
 	printf ("%d\n", D1.item);
-} */
+}
 
 int main () {
 
-	Deque** D1;
+	Deque *D1;
 	D1 = createItem();
-	
-	insereDireita (&D1, 100);
-	
+	insereDireita (D1, 100);
+	printf ("%d\n", D1->item);	
 
-	//imprime (*D1);
+	imprime (*D1);
 	return 0;
 }
