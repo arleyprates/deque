3c38b9b10e7c594c20b2e58c5e08d6b86952cd04
Arley Prates <arleyprates@gmail.com>
2014-07-10 04:52:56 -0300

    Descobri que ta errado e que tenho que refazer o processo de inserir no primeiro item para debugar esse erro

--- untiledd.c
+++ untiledd.c
@@ -20,17 +20,21 @@ Deque* createItem () {
 int insereDireita (Deque *D1, int item) {
 	Deque *novo, *aux;
 	if (D1->direita == NULL && D1->item == -1) {
-		printf ("D1 %p\n", D1);
 		novo = createItem ();
 		D1->direita = novo;
 		D1->direita->item = item;
-		D1->direita->esquerda = NULL;
+		//D1->direita->esquerda = NULL;
+
 	} else {
-		aux = D1->direita->direita;
+		//printf ("D1->direita ELSE %p\n", D1->direita);
+		aux = createItem();
+		D1->direita->direita = aux;
 		novo = createItem();
 		D1->direita = novo; // Aponto para um endereco valido 
 		D1->direita->item = item;
 		D1->direita->esquerda = aux; // Endereco Aponta para um endereco valido
+		//printf("novo ELSE: %p\n", novo);
+		printf("D1->direita->esquerda ELSE: %p\n", D1->direita->esquerda);
 	}
 	printf("RETORNANDO 0\n");
 	return 0;
@@ -38,10 +42,12 @@ int insereDireita (Deque *D1, int item) {
 
 
 void imprime (Deque D1) {
-	while (D1.direita != NULL) {
-		printf ("%d ", (D1.direita)->item);
-		D1.direita = D1.direita->esquerda;
-		
+	//printf("D1.esquerda IMPRIME: %p\n", D1.esquerda);
+	printf("D1.direita->esquerda IMPRIME: %p\n", D1.direita->esquerda);
+	while (D1.direita->esquerda != NULL) {
+		printf ("%d ", D1.direita->item);
+		D1.direita = D1.direita->esquerda;	
+
 	}
 }
 
@@ -50,14 +56,14 @@ int main () {
 	Deque *D1, *aux;
 	D1 = createItem();
 	aux = D1;
-	printf ("D1 %p\n", D1);
+	//printf ("D1 %p\n", D1);
 	insereDireita (D1, 100);
 	printf("	Foram ITEM: %d\n", D1->direita->item);
 	insereDireita (D1, 200);
 	printf("	Fora ITEM: %d\n", D1->direita->item);
 	insereDireita (D1, 300);
 	printf("	Fora  ITEM: %d\n", D1->direita->item);	
-	printf ("D1 %p\n", D1);
-	//imprime (*aux);
+	//printf ("D1 %p\n", D1);
+	imprime (*D1);
 	return 0;
 }
