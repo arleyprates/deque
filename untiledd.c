304c80f2b971778bc90c6ff82e340d57bb8697dd
Arley Prates <arleyprates@gmail.com>
2014-07-10 12:04:30 -0300

    Ajustes

--- untiledd.c
+++ untiledd.c
@@ -19,29 +19,45 @@ Deque* createItem () {
 
 int insereDireita (Deque *D1, int item) {
 	Deque *novo, *aux;
-	if (D1->direita == NULL && D1->item == -1) {
-		novo = createItem ();
-		D1->direita = novo;
-		D1->direita->item = item;
-		//D1->direita->esquerda = NULL;
+		
+	if (!D1) {
+		printf("EXISTE\n");
+	}
 
-	} else {
+	novo = createItem ();
+	*D1 = *novo;
+	printf("D1->item %d\n", D1->item);
+	D1->item = item;
+	printf("D1: %p\n", D1);
+	printf("D1->direita %p\n", D1->direita);
+	D1->direita = D1;
+	printf("D1->direita %p\n", D1->direita);
+	
+	/*
+	else {
 		//printf ("D1->direita ELSE %p\n", D1->direita);
-		aux = createItem();
-		D1->direita->direita = aux;
+		//aux = createItem();
+		//if (D1->direita == NULL)
 		novo = createItem();
-		D1->direita = novo; // Aponto para um endereco valido 
+		*D1->direita = *novo->esquerda;
 		D1->direita->item = item;
-		D1->direita->esquerda = aux; // Endereco Aponta para um endereco valido
+
+		printf("D1->item: %d e D1->direita->item: %d\n", D1->item, D1->direita->item);
+		//novo = createItem();
+		
+		//D1->direita = novo; // Aponto para um endereco valido 
+		//D1->direita->item = item;
+		//D1->direita->esquerda = aux; // Endereco Aponta para um endereco valido
+		
 		//printf("novo ELSE: %p\n", novo);
 		printf("D1->direita->esquerda ELSE: %p\n", D1->direita->esquerda);
-	}
+	}*/
 	printf("RETORNANDO 0\n");
 	return 0;
 }
 
 
-void imprime (Deque D1) {
+/*void imprime (Deque D1) {
 	//printf("D1.esquerda IMPRIME: %p\n", D1.esquerda);
 	printf("D1.direita->esquerda IMPRIME: %p\n", D1.direita->esquerda);
 	while (D1.direita->esquerda != NULL) {
@@ -50,20 +66,18 @@ void imprime (Deque D1) {
 
 	}
 }
-
+*/
 int main () {
 
-	Deque *D1, *aux;
-	D1 = createItem();
-	aux = D1;
-	//printf ("D1 %p\n", D1);
-	insereDireita (D1, 100);
-	printf("	Foram ITEM: %d\n", D1->direita->item);
-	insereDireita (D1, 200);
-	printf("	Fora ITEM: %d\n", D1->direita->item);
+	Deque D1;
+	insereDireita (&D1, 100);
+	//printf("	Fora ITEM: %d\n", D1.item); 
+	insereDireita (&D1, 200);
+	//printf("	Fora ITEM: %d\n", D1.item);
+	/*
 	insereDireita (D1, 300);
 	printf("	Fora  ITEM: %d\n", D1->direita->item);	
 	//printf ("D1 %p\n", D1);
-	imprime (*D1);
+	imprime (*D1); */
 	return 0;
 }
