c445e4e9b0824ff71f3b9ce6bc68b8561997ed03
Arley Prates <arleyprates@gmail.com>
2014-07-12 00:33:21 -0300

    Deque Dinamico | Insere Direita OK | Imprime OK | Inicia Deque OK

---
+++ deque-dinamic.c
@@ -0,0 +1,71 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct deque {
+	int item;
+	struct deque *direita, *esquerda;
+}Deque;
+
+
+int insereDireita (Deque *D1, int item) {
+	Deque *aux;
+	aux = D1->direita;
+	if (D1->direita != NULL) {
+		while (D1->direita != NULL) {
+			if (D1->direita->direita == NULL)
+				break;
+			D1->direita = D1->direita->direita;
+			if (D1->direita->direita == NULL)
+				break;
+		}
+		D1->direita->direita = (Deque*) malloc (sizeof (Deque));
+		if (D1->direita->direita == NULL)
+			printf ("Erro malloc!\n");
+		D1->direita->direita->item = item;
+		D1->direita->direita->esquerda = D1->direita;
+		D1->direita = aux;
+		printf("RETORNO2\n");
+		return 0;
+	}
+
+	D1->direita = (Deque*) malloc (sizeof (Deque));
+	if (D1->direita == NULL)
+		printf ("Erro malloc!\n");	
+	D1->direita->item = item;
+	D1->direita->esquerda = D1;
+	printf("RETORNO1 \n");
+	return 0;
+}
+
+void imprime (Deque D1) {
+	while (D1.direita != NULL) {
+		printf(" %d", D1.direita->item);
+		D1.direita = D1.direita->direita;
+	}
+	printf("\n");
+}
+
+Deque* initDeque () {
+	Deque *no;
+	
+	no = (Deque*) malloc (sizeof (Deque));
+	if (no == NULL)
+		printf ("Erro malloc !!!\n");
+
+	return no;
+}
+int main () {
+	Deque *D1;
+	
+	D1 = initDeque ();
+
+	
+	insereDireita (D1, 100);
+	insereDireita (D1, 200);
+	insereDireita (D1, 300);
+	insereDireita (D1, 400);
+	insereDireita (D1, 500);
+	imprime (*D1);
+	
+	return 0;
+}
